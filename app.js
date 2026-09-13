8c69e6e910d0c102ed1d4b202247be1952bd8478
marcastorre1 <gygygaga645@gmail.com>
2026-09-13 16:13:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ tg.expand();
 // ============================================
 
 // Твой ник в Telegram БЕЗ @
-const MANAGER_USERNAME = 'sashalppro';
+const MANAGER_USERNAME = 'pizdezix';
 
 // Курс: сколько MDL в 1 EUR (поменяй на актуальный!)
 const EUR_RATE = 19.5;
